471324a5d0423314c22aa2d09615db648b021d4b
suyashmishra9 <suyashmishra1008@gmail.com>
2023-01-11 15:42:02 +0530

    first commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link_tree ../builds
+//= link application.bootstrap.scss
+//= require rails-ujs
+//= require activestorage
